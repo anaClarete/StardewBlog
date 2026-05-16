93d7531aa43d70467784f87628abf4aa44b80095
anaClarete <ana.clarete@sptech.school>
2026-05-16 19:11:15 -0300

    Adicionado checkboxs nos itens

--- banco/Script.sql
+++ banco/Script.sql
@@ -1,11 +1,3 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
 CREATE DATABASE stardewblog;
 USE stardewblog;
 
@@ -43,4 +35,6 @@ CONSTRAINT ctFkPacote
 FOREIGN KEY (fkPacote) 
 REFERENCES pacotes(idPacote)
 );
+
+
 
